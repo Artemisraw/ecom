b8f87dbd91ad093df459396cecd1544c6fce10e1
Victor Mbugua <127848399+Amison23@users.noreply.github.com>
2024-02-02 12:56:48 +0300

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.home, name='home')
-        ]
+    path('', views.home, name='home'),
+]
+
+urlpatterns += [
+    path(r'login', views.login, name='login'),
+    path(r'register', views.register, name='register'),
+]
+
+urlpatterns += [
+]
