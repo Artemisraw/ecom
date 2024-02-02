d85d867ef032b81cc6bc2c0f8e0c5c2c3ac00974
Victor Mbugua <127848399+Amison23@users.noreply.github.com>
2024-02-02 12:56:17 +0300

    Update views.py

--- store/views.py
+++ store/views.py
@@ -6,3 +6,11 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'home.html', {'products':products})
+
+def login(request):
+    login_url = '../templates/registration/login.html/'
+    return render(request, login_url)
+
+def register(request):
+    register_url = '../templates/registration/register.html/'
+    return render(request, register_url)
