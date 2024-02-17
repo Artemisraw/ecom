77ac835f07b1c134e0f9c41f5521ad1a7f84bd01
Victor Mbugua <127848399+Amison23@users.noreply.github.com>
2024-02-18 00:25:15 +0300

    frontend changes....cart page, payment, profile still left to do

--- cart/views.py
+++ cart/views.py
@@ -1,17 +1,49 @@
 from django.shortcuts import render, get_object_or_404
 from .cart import Cart
+from django.contrib import messages
 from store.models import Product
 from django.http import JsonResponse
 # Create your views here.
 
 def cart_sum(request):
-    return render(request, 'cart_sum.html', {})
+    # get Cart
+    cart = Cart(request)
+    cart_products = cart.get_prod
+    quantities = cart.get_quantity
+    total = cart.cart_total
+
+    return render(request, 'cart_sum.html', {"cart_products": cart_products, 'quantities': quantities, 'total':total})
 
 def cart_delete(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # get stuff
+        product_id = request.POST.get('product_id')
+
+        # delete funtion
+        cart.delete(product=product_id)
+
+        response = JsonResponse({'product': product_id})
+        messages.success(request, ("Item deleted from cart"))
+        return response
+    
 
 def cart_update(request):
-    pass
+    cart = Cart(request)
+    if request.POST.get('action') == 'post':
+        # get stuff
+        product_id = request.POST.get('product_id')
+        product_quantity = request.POST.get('product_quantity')
+
+        cart.update(product=product_id, quantity=product_quantity)
+
+        response = JsonResponse({'Quantity': product_quantity})
+        messages.success(request, ("Your cart has been updated"))
+        return response
+
+
+
+
 
 def cart_add(request):
     cart = Cart(request)
@@ -19,13 +51,19 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # get stuff
         product_id = request.POST.get('product_id')
+        product_quantity = request.POST.get('product_quantity')
 
         # find the product
         product = get_object_or_404(Product, id=product_id)
 
         # store in settion 
-        cart.add(product=product)
+        cart.add(product=product, quantity=product_quantity)
+
+        # get quantity number
+        cart_quantity = cart.__len__()
 
         # return response
-        response = JsonResponse({'Product Name: ': product.name})
+        # response = JsonResponse({'Product Name: ': product.name})
+        response = JsonResponse({'Quantity: ': cart_quantity})
+        messages.success(request, ("Product added to cart"))
         return response
--- store/urls.py
+++ store/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
@@ -7,8 +7,14 @@ urlpatterns = [
     path('logout/', views.logout_user, name='logout'),
     path('about/', views.about, name='about'),
     path('register/', views.register_user, name='register'),
+    path('update_user/', views.update_user, name='update_user'),
+    path('update_password/', views.update_password, name='update_password'),
+    path('update_info/', views.update_info, name='update_info'),
     path('product/<int:pk>', views.product, name='product'),
     path('category/<str:cat>', views.category, name='category'),
+    path('category_summary/', views.category_summary, name='category_summary'),
+    path('search/', views.search, name='search'),
    
 ]
+
 
